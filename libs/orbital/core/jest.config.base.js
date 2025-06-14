65fcd9537df11c16f7dd30a5ed7c149e0fe7628f
Artur Moczulski <artur.moczulski@gmail.com>
2025-06-14 10:22:00 -0700

    chore(repo): remove deprecated @orbital/core package and associated files

--- libs/orbital/core/jest.config.base.js
+++
@@ -1,8 +0,0 @@
-/** Base Jest configuration */
-module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "node",
-  moduleFileExtensions: ["ts", "js", "json", "node"],
-  passWithNoTests: true,
-  setupFilesAfterEnv: ["<rootDir>/tests/jest.setup.js"],
-};
--- libs/orbital/core/tests/unit/jest.setup.unit.js
+++
@@ -1,2 +0,0 @@
-// Jest setup for unit tests
-// Add unit-test-specific configuration or global mocks here
